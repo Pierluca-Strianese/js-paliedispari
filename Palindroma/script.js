549d631b18ce30196c6774d5c2376dec20655f3c
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-21 10:06:17 +0200

    fix function Palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,22 +1,20 @@
 const word = prompt('Scrivi una parola: ');
 
-reverseString(word)
-
-palindroma (word,newWord);
-
-function palindroma (word, newWord) {
-    if ( word == newWord) {
-        console.log ('La parola è palindroma');
-    }
-    else {
-        console.log ('La parola non è palindroma');
-    }
+if (Palindroma(word)) {
+    console.log ('La parola è palindroma');
+}
+else {
+    console.log ('La parola NON è palindroma');
 }
 
-function reverseString(word) {
+
+function Palindroma () {
     let newWord = '';
     for (let i = word.length - 1; i >= 0; i--) { 
         newWord += word[i];
     }
-    return newWord;
+
+    if (word == newWord) {
+        return true;
+    }
 }
